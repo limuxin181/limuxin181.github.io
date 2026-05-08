e3abce74bc5eef4bac88279508eb0c1815a747dc
limuxin181 <limuxin181@gmail.com>
2026-05-08 21:14:56 +0800

    fix github pages style issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  server: {
-    port: 3000
-  }
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        detail: resolve(__dirname, 'project-detail.html'),
+      },
+    },
+  },
 })
